34218741921e4adb0fcbb49e82af57e5ce1a548e
Adiel Ben Meir <adiel1892@gmail.com>
2023-08-06 18:31:28 +0300

    adding link to

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,5 +35,12 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+
+    end
+
 
 end
