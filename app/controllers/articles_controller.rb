b491e8b082214ce2de5ce7151463c97cd3c66381
Adiel Ben Meir <adiel1892@gmail.com>
2023-08-06 17:16:14 +0300

    Messaging - validation and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,5 +7,19 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to @article
+        else 
+            render "new", status: :unprocessable_entity
+        end
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles , only: [:show , :index]
+  resources :articles , only: [:show , :index, :new , :create]
 
 end
